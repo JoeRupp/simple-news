c774d69e8a04478528a14318e12a5959fad2f3f8
Joe Rupp <joerpp8@gmail.com>
2022-07-25 17:13:07 -0600

    Add initial app structure
    Import required components into app and create routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import NavBar from "./NavBar";
+import HomePage from "./HomePage";
+import SubSectionPage from "./SubSectionPage";
+import ArticlePage from "./ArticlePage";
+import NoMatch from "./NoMatch";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
-  return <h1>Simple News</h1>;
-}
+const App = () => {
+  return (
+    <main>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="section/:sectionName" element={<SubSectionPage />} />
+        <Route exact path="article/:articleId" element={<ArticlePage />} />
+        <Route exact path="*" element={<NoMatch />} />
+      </Routes>
+    </main>
+  );
+};
 
 export default App;
