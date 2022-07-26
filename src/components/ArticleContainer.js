715add38d3b74a4e2356fa9f27c1b9acd4a9480a
Joe Rupp <joerpp8@gmail.com>
2022-07-26 15:27:12 -0600

    Add news container and preview styling

--- src/components/ArticleContainer.js
+++ src/components/ArticleContainer.js
@@ -1,20 +1,29 @@
 import React from "react";
 import ArticlePreview from "./ArticlePreview";
+import styled from "styled-components";
 
 const ArticleContainer = ({ articles }) => {
-  const articleList = articles.map((article) => {
+  const articleList = articles.map((article, index) => {
     return (
       <ArticlePreview
+        image={article.multimedia[0]}
         title={article.title}
         abstract={article.abstract}
         section={article.section}
         subsection={article.subsection}
-        key={Math.random()}
+        key={index}
+        id={index}
       />
     );
   });
 
-  return <div>{articleList}</div>;
+  return <Container>{articleList}</Container>;
 };
 
 export default ArticleContainer;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto;
+`;
--- src/components/ArticlePreview.js
+++ src/components/ArticlePreview.js
@@ -1,16 +1,42 @@
 import React from "react";
+import styled from "styled-components";
 
-const ArticlePreview = ({ title, abstract, section, subsection }) => {
+const ArticlePreview = ({ title, abstract, section, subsection, image }) => {
   return (
-    <div>
-      <h3>{title}</h3>
-      <p>{abstract}</p>
-      <p>{`${section} | ${subsection}`}</p>
-    </div>
+    <NewsCard>
+      <Img src={image.url} alt={image.caption} />
+      <InfoContainer>
+        <h3>{title}</h3>
+        <p>{abstract}</p>
+        <p>{`${section}  ${subsection}`}</p>
+      </InfoContainer>
+    </NewsCard>
   );
 };
 
 export default ArticlePreview;
+
+const NewsCard = styled.div`
+  padding: 2.5%;
+  display: flex;
+  align-items: flex-start;
+  &:hover {
+    cursor: pointer;
+    color: #35aad6;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 8px;
+    border: 0;
+  }
+`;
+
+const Img = styled.img`
+  width: 375px;
+  height: auto;
+  object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+  padding-left: 5%;
+`;
 
 // title={article.title}
 //         abstract={article.abstract}
